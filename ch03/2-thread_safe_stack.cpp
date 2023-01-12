b836b307c72c4e93596a14e545af55a4a1169d46
ViniBR01 <vinicius@rice.edu>
2023-01-11 21:09:09 -0600

    Finish implementation of the threadsafe_stack class

--- ch03/2-thread_safe_stack.cpp
+++ ch03/2-thread_safe_stack.cpp
@@ -1,19 +1,56 @@
 #include <exception>
 #include <iostream>
 #include <memory>
+#include <mutex>
+#include <stack>
 
-struct empty_stack: std::exception {
-  const char* what() const noexcept;
+struct empty_stack : std::exception {
+  char * what () {
+    char * message = (char *) "The stack was empty";
+    return message;
+  }
 };
 
 template<typename T>
 class threadsafe_stack {
+private:
+  std::stack<T> data;
+  mutable std::mutex m;
 public:
-  threadsafe_stack();
-  threadsafe_stack(const threadsafe_stack&);
+  threadsafe_stack() {}
+  threadsafe_stack(const threadsafe_stack& other) {
+    std::lock_guard<std::mutex> lock(other.m);
+    data = other.data;
+  }
   threadsafe_stack& operator=(const threadsafe_stack&) = delete;
-  void push(T new_value);
-  std::shared_ptr<T> pop();
-  void pop(T& value);
-  bool empty() const;
+  void push(T new_value) {
+    std::lock_guard<std::mutex> lock(m);
+    data.push(std::move(new_value));
+  }
+  std::shared_ptr<T> pop() {
+    std::lock_guard<std::mutex> lock(m);
+    if(data.empty()) throw empty_stack();
+    std::shared_ptr<T> const res(std::make_shared<T>(data.top()));
+    data.pop();
+    return res;
+  }
+  void pop(T& value) {
+    std::lock_guard<std::mutex> lock(m);
+    if(data.empty()) throw empty_stack();
+    value=data.top();
+    data.pop();
+  }
+  bool empty() const {
+    std::lock_guard<std::mutex> lock(m);
+    return data.empty();
+  }
 };
+
+int main() {
+  threadsafe_stack<int> my_stack;
+  std::cout << my_stack.empty() << std::endl;
+  my_stack.push(3);
+  std::cout << my_stack.empty() << std::endl;
+  std::shared_ptr<int> value;
+  value = my_stack.pop();
+}
